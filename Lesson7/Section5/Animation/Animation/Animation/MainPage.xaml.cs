a23570ae664db2d9f024c0bad248db3698e8460b
Onur Gumus <onur.gumus@emaratech.ae>
2018-11-18 13:15:45 +0400

    Lesson7 completed

---
+++ Lesson7/Section5/Animation/Animation/Animation/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Animation
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private  async void StartButton_Clicked(object sender, EventArgs e)
+        {
+            await image.RotateTo(360, 2000);
+            image.Rotation = 0;
+        }
+
+
+
+
+
+
+    }
+}
