aefd15d33ea4e1b7e615db433ed9c9691dd8c18e
Onur Gumus <onur.gumus@emaratech.ae>
2018-11-18 15:17:04 +0400

    Lab4 completed

---
+++ Lesson8/Lab4/Lab4/Lab4.Server/Controllers/GameScoreController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab4.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab4.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameScoreController : ControllerBase
+    {
+
+        static volatile GameScore currentScore;
+
+        static readonly object locker = new object();
+        // GET api/values
+        [HttpGet]
+        public ActionResult<GameScore> Get()
+        {
+            lock (locker)
+            {
+                return currentScore;
+            }
+        }
+
+
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] GameScore value)
+        {
+            lock (locker)
+            {
+                if (currentScore == null)
+                    currentScore = value;
+                else
+                {
+                    if (currentScore.BestAttempt >= value.BestAttempt)
+                    {
+                        currentScore = value;
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ Lesson8/Lab4/Lab4/Lab4.Shared/GameScore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lab4.Shared
+{
+    public class GameScore
+    {
+        public string PlayerName { get; set; }
+        public int BestAttempt { get; set; }
+
+        public override string ToString()
+        {
+            return $"Player {PlayerName} made the highest score as : {BestAttempt} attempts!";
+        }
+    }
+}
